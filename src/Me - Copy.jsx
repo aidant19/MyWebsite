6197460a29c87e204547aa4c799dcb24b3f6920c
aidant19 <aidant19@gmail.com>
2023-09-25 18:15:31 -0400

    Spent hours on this changing text thing

--- src/Me - Copy.jsx
+++ src/Me - Copy.jsx
@@ -1,10 +1,9 @@
 import headshot from "./assets/headshot_edited.jpg";
-import React from "react";
 import "./Me.css";
 
 class Me extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.descriptors = [
       "Software Engineer",
       "Data Engineer",
@@ -12,37 +11,27 @@ class Me extends React.Component {
       "Ultimate Frisbee God",
       "Speed Demon in the Rink",
     ];
-    this.state = {
-      count: 0,
-    };
+    this.descriptorIdx = 0;
   }
 
-  setCount() {
-    this.setState({ count: (this.state.count + 1) % this.descriptors.length });
+  descriptorCounter() {
+    ++this.descriptorIdx % this.descriptors.length;
   }
 
-  componentDidMount() {
-    this.countID = setInterval(
-      () => this.setCount(),
-      3000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.countID);
-  }
+  //useEffect = (() => {
+    //const interval = setInterval(() => handleTextChange(), 1000);
+    //return () => clearInterval(interval);
+  //}, []);
 
   render() {
     return (
       <div>
         <div className="nav-bar">
-          <h1>Cool Guy</h1>
+          <h1>test</h1>
         </div>
         <div className="main-content">
           <h1 className="headshot-text">Aidan Tokarski</h1>
-          <h2 className="headshot-descriptor">
-            {this.descriptors[this.state.count]}
-          </h2>
+          {/*<h2 className="headshot-descriptor">{this.descriptors[this.descriptorIdx]}</h2>*/}
           <img src={headshot} className="headshot" />
         </div>
       </div>
