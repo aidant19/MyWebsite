e158a053ebe207073661540031b076dc08168948
aidant19 <aidant19@gmail.com>
2023-09-25 14:56:09 -0400

    Removing Greeting.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Greeting from './Greeting.jsx'
 import Me from './Me.jsx'
 import './index.css'
 
