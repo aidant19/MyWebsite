8151b61e555826ffd3ee2c835868a5669f0e9b8a
aidant19 <aidant19@gmail.com>
2023-10-30 10:28:54 -0400

    Latest update with multi-page format

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,9 +1,15 @@
 import "./index.css";
+import rscWins from "./assets/rsc_wins.PNG";
+import mvp from "./assets/mvp.PNG";
+import topFranchise from "./assets/top_franchise.PNG";
 
 const Projects = () => {
   return (
     <div>
         <p1>Projects</p1>
+        <img src={rscWins}/>
+        <img src={mvp}/>
+        <img src={topFranchise}/>
     </div>
   );
 };
