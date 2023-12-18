826f6eb0c3f814f6bb551902a97b93049595b6e9
aidant19 <aidant19@gmail.com>
2023-12-18 16:50:10 -0500

    Sign aesthetic overhaul and projects page refinement

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Helmet } from "react-helmet";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Helmet>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0"
-      ></meta>
-    </Helmet>
     <BrowserRouter>
       <App />
     </BrowserRouter>
